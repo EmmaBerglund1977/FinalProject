d2877c7b51d0804f24a6eee42594eba440c444c2
Emma <emma.z.berglund@teliacompany.com>
2019-11-29 16:10:08 +0100

    Final

--- code/script.js
+++ code/script.js
@@ -1,8 +1,5 @@
-// API key: yy4GMy82PIQTTnCdF62v60K7W9ZmnFTQ
-// SEARCH  https://developers.giphy.com/docs/api/endpoint/#search
-// https://developers.giphy.com/explorer/
 
-// RESET
+// RESET 
 const searchForm = document.getElementById("searchForm");
 const searchInput = document.getElementById("searchInput");
 
@@ -12,9 +9,10 @@ const refreshPage = () => {
 };
 
 // onsubmit triggers on button press and when using the enter key
-searchForm.onsubmit = event => {
-  event.preventDefault(); // för att inte formet ska ladda om sidan, det är standard behaviour
 
+searchForm.onsubmit = event => {
+  event.preventDefault();
+  
   let searchValue = searchInput.value;
   console.log("searchValue", searchValue);
   handleSearch(searchValue);
@@ -31,7 +29,7 @@ const handleSearch = searchValue => {
     .then(response => response.json())
     .then(json => {
       console.log("json", json);
-      // loopa igenom arrayen och skapa en img tag för varje bild
+      // loop the array snd create img tags
       json.data.forEach(item => {
         searchResponsesMobile.innerHTML += `<img src=${item.images.fixed_width.url} />`;
       });
@@ -40,7 +38,7 @@ const handleSearch = searchValue => {
     .then(response => response.json())
     .then(json => {
       console.log("json", json);
-      // loopa igenom arrayen och skapa en img tag för varje bild
+        // loop the array snd create img tags
       json.data.forEach(item => {
         searchResponses.innerHTML += `<img src=${item.images.fixed_width.url} />`;
       });
